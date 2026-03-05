9cf3b477e7e3d483b0855a3de18dc0497f9a5e54
JayMossy <berryjadyn@gmail.com>
2026-03-04 22:25:22 -0700

    physics added, updated to match (#1)

--- src/js/entities/Player.js
+++ src/js/entities/Player.js
@@ -1,7 +1,28 @@
-export const player = {
-    x: 450,
-    y: 300,
-    size: 24,
-    vx: 260,
-    vy: 260,
-};
+import { Vec2 } from "../utils/Vec2.js";
+
+export function createPlayer(x, y) {
+    return {
+        pos: new Vec2(x, y),
+        vel: new Vec2(0, 0),
+        size: 20,
+        accel: 1000,
+        drag: 0.8,
+        maxSpeed: 300,
+        aimAngle: 0
+    };
+}
+
+// Player.js should export a function like:
+
+// createPlayer(x, y)
+
+// Return a player object containing:
+
+// position (pos) and velocity (vel) (Vec2)
+
+// size
+
+// movement stats: accel, drag, maxSpeed
+
+// aiming: aimAngle (number)
+
--- src/js/utils/Vec2.js
+++ src/js/utils/Vec2.js
@@ -1,1 +1,36 @@
-let dir = mousePos.sub(player.pos).normalize();
+// src/js/utils/Vec2.js
+export class Vec2 {
+    constructor(x = 0, y =0) {
+        this.x = x;
+        this.y = y;
+    }
+    len() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    normalize() {
+        let length = this.len();
+        if (length > 0) {
+            this.x /= length;
+            this.y /= length;
+        }
+        return this;
+    }
+    clone() {
+        return new Vec2(this.x, this.y);
+    }
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        return this;
+    }
+    sub(v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        return this;
+    }
+    scale(s) {
+        this.x *= s;
+        this.y *= s;
+        return this;
+    }
+}
